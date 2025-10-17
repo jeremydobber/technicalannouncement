b3b599ae64a2596497c2f693772c0eac53f43d39
Simon Fouilleul <sfouilleul@babylone.tailc7756c.ts.net>
2025-10-17 18:15:54 +0200

    added index file to root

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright since 2025 Jeremy Dobberman
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade to newer
+ * versions in the future. If you wish to customize it for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    Jeremy Dobberman <yellowyankee@proton.me>
+ * @copyright Since 2025 Jeremy Dobberman
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
